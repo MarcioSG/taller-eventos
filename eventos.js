a1db5974f3c5fc8dc79eb7198619dc0536c51c8d
MarcioSG <msouzagoy@gmail.com>
2023-08-29 01:38:50 -0300

    NuevosCommit

--- eventos.js
+++ eventos.js
@@ -2,3 +2,9 @@
 function showAlert() {
     alert("Hola");
 }
+
+const eldiv = document.getElementById("soyeldiv");
+
+eldiv.addEventListener('click', function () {
+    alert('Hola! Soy el div');
+});
